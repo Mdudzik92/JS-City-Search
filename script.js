952fc3ddc4a8622a8bd1cda96d1815a47696b91e
Matt Dudzik <mdudzik92@gmail.com>
2021-08-06 01:18:46 -0500

    Added regex to add commas to population numbers

--- script.js
+++ script.js
@@ -2,7 +2,6 @@ const endpoint =
 	"https://gist.githubusercontent.com/Miserlou/c5cd8364bf9b2420bb29/raw/2bf258763cdddd704f8ffd3ea9a3e81d25e2c6f6/cities.json";
 
 const cities = [];
-
 fetch(endpoint)
 	.then((blob) => blob.json())
 	.then((data) => cities.push(...data));
@@ -14,16 +13,31 @@ function findMatches(wordToMatch, cities) {
 	});
 }
 
+function numberWithCommas(x) {
+	return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+}
+
 function displayMatches() {
 	const matchArray = findMatches(this.value, cities);
 	const html = matchArray
 		// .map returns an array, so using .join("") to turn it all into one string
 		.map((place) => {
-			return `<li>
-                  <span class="name">${place.city}, ${place.state}</span>
-                  <span class="population">${place.population}</span>
-                </li>
-                `;
+			// Creating a new regex to match the city name
+			const regex = new RegExp(this.value, "gi");
+			const cityName = place.city.replace(
+				regex,
+				`<span class="hl">${this.value}</span>`,
+			);
+			const stateName = place.state.replace(
+				regex,
+				`<span class="hl">${this.value}</span>`,
+			);
+			return `
+      <li>
+        <span class="name">${cityName}, ${stateName}</span>
+        <span class="population">${numberWithCommas(place.population)}</span>
+      </li>
+    `;
 		})
 		.join("");
 	suggestions.innerHTML = html;
